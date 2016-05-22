f7088a2fd599e88209fbc20389c9e856c3a5082e
samira <s.allahverdiyeva@gmail.com>
2016-05-21 18:11:20 -0700

    added tasks

--- Eloquent-JS/Chapter-4/List.js
+++ Eloquent-JS/Chapter-4/List.js
@@ -1,3 +1,11 @@
+/*Objects, as generic blobs of values, can be used to build all sorts of data structures. 
+A common data structure is the list (not to be confused with the array). A list is a nested 
+set of objects, with the  rst object holding a reference to the second, the second to the third, and so on.
+Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as argument,
+and write a listToArray function that produces an array from a list. Also write the helper functions prepend, 
+which takes an element and a list and creates a new list that adds the element to the front of the input list, 
+and nth, which takes a list and a number and returns the element at the given position
+in the list, or undefined when there is no such element.*/
 function arrayToList(array) {
   var list = null;
   for (var i = array.length - 1; i >= 0; i--)
--- Eloquent-JS/Chapter-4/deepEqual.js
+++ Eloquent-JS/Chapter-4/deepEqual.js
@@ -1,3 +1,8 @@
+// The == operator compares objects by identity. But sometimes, you would prefer to compare the values of their actual properties.
+// Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects with the same properties whose values are also equal when compared with a recursive call to deepEqual.
+// To  nd out whether to compare two things by identity (use the === operator for that) or by looking at their properties, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account:
+// by a historical accident, typeof null also produces "object".
+
 function deepEqual(a, b) {
   if (a === b) return true;
   
